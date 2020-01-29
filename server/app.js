62b1a67be728409633f38533e53b878bb9bcc7f3
Joshua Seigler <joshua@seigler.net>
2020-01-29 00:24:42 -0500

    feat(server): connect to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+require('dotenv-safe').config();
 
 const schema = require('./schema/schema');
 
 const app = express();
+
+mongoose.connect(process.env.connectionString, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+mongoose.connection.once('open', () => {
+  console.log('Connected to database.');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
