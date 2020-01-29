27a5900978b14e197b2ac5bd8484e5af28f4746a
Joshua Seigler <joshua@seigler.net>
2020-01-28 23:17:10 -0500

    feat(server): add dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema
 } = graphql;
+
+// temp data
+const books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -22,7 +30,7 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { GraphQLString } },
       resolve (parent, { id }) {
-        // code to retrieve data from db or other source
+        return _.find(books, { id });
       }
     }
   }
