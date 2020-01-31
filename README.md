7bc3484b7146ab1e8970b97d20eea0d58b6c4160
Joshua Seigler <joshua@seigler.net>
2020-01-30 22:46:01 -0500

    Client initial commit

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 Repository for following https://www.youtube.com/watch?v=ed8SzALpx1Q
 
+I stuck with the video for the server, but did my own thing for the client.
+
 ## Usage
 
+### Server
 ```
+cd server
+npm install
+npm start
+```
+### Client
+```
+cd client
 npm install
 npm start
 ```
---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createClient, defaultExchanges, Provider } from 'urql';
+import App from './app';
+
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+  exchanges: defaultExchanges
+});
+
+const app = (
+  <Provider value={client}>
+    <App />
+  </Provider>
+);
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(app, rootElement);
